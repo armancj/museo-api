85ba82f1e6aabe9beca2f5cdba1089fd5f6d4b7d
mandi <armancanjer@gmail.com>
2024-11-12 17:57:52 -0500

    feat/add-institution-module: add cultrure heritage

--- src/cultural-heritage-property/schema/cultural-heritage-property.ts
+++ src/cultural-heritage-property/schema/cultural-heritage-property.ts
@@ -1,22 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Model } from 'mongoose';
-import {ProducerAuthorRecord} from "../producer-author-record/models/producer-author-record.models";
-import {ProducerAuthorRecordSchema} from "../producer-author-record/schema/producer-author-record";
-import {BaseSchema, BaseSchemaFactory} from "../../common/schema/base.schema";
+import { ProducerAuthorRecord } from '../producer-author-record/models/producer-author-record.models';
+import { ProducerAuthorRecordSchema } from '../producer-author-record/schema/producer-author-record';
+import { BaseSchema, BaseSchemaFactory } from '../../common/schema/base.schema';
 
-export type CulturalHeritagePropertyDocument = HydratedDocument<CulturalHeritageProperty>;
+export type CulturalHeritagePropertyDocument =
+  HydratedDocument<CulturalHeritageProperty>;
 
 @Schema()
 export class CulturalHeritageProperty extends BaseSchema {
-    @Prop({ type: ProducerAuthorRecordSchema })
-    producerAuthor: ProducerAuthorRecord;
-    // Añade aquí las otras propiedades de CulturalPropertiesModel implementadas
+  @Prop({ type: ProducerAuthorRecordSchema })
+  producerAuthor: ProducerAuthorRecord;
+  // Añade aquí las otras propiedades de CulturalPropertiesModel implementadas
 }
 
-
-export const CulturalHeritagePropertySchema = SchemaFactory.createForClass(CulturalHeritageProperty);
+export const CulturalHeritagePropertySchema = SchemaFactory.createForClass(
+  CulturalHeritageProperty,
+);
 CulturalHeritagePropertySchema.add(BaseSchemaFactory);
 
-
 export const CulturalHeritagePropertyEntity = 'CulturalHeritageProperty';
-export type CulturalHeritagePropertyModel = Model<CulturalHeritagePropertyDocument>;
+export type CulturalHeritagePropertyModel =
+  Model<CulturalHeritagePropertyDocument>;
