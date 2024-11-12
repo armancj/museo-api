75c6bf8458211fb14b68625fd8b87620e1e3a2f9
armancj <76697120+armancj@users.noreply.github.com>
2024-11-12 12:08:22 -0500

    feat: delete avatar file in user controller

--- src/cultural-heritage-property/producer-author-record/models/producer-author-record.models.ts
+++ src/cultural-heritage-property/producer-author-record/models/producer-author-record.models.ts
@@ -2,12 +2,12 @@ export interface ProducerAuthorRecord {
   producerAuthorNames: string;
   street: string;
   number: string;
-  betweenStreet1: string; // Entre Calle 1
-  betweenStreet2: string; // Entre Calle 2
-  district: string; // Reparto
-  locality: string; // Localidad
-  municipality: string; // Municipio
-  province: string; // Provincia
-  institutionalHistory?: string; // Historia institucional/ Reseña biográfica del productor / autor del objeto (campo no obligatorio)
-  objectEntryHistory?: string; // Historia relacionada con la forma de ingreso del objeto (campo no obligatorio)
+  betweenStreet1: string;
+  betweenStreet2: string;
+  district: string;
+  locality: string;
+  municipality: string;
+  province: string;
+  institutionalHistory?: string;
+  objectEntryHistory?: string;
 }
