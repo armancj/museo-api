bd385bccb5ee68204d522c91bac14da2dee99b34
mandi <armancanjer@gmail.com>
2024-11-12 19:18:29 -0500

    feat/add-institution-module: add cultural embed property document heritage

--- src/cultural-heritage-property/cultural-record/Schema/extreme-dates.schema.ts
+++ src/cultural-heritage-property/cultural-record/Schema/extreme-dates.schema.ts
@@ -2,5 +2,5 @@ import { Schema as MongooseSchema } from 'mongoose';
 
 export const ExtremeDatesSchema = new MongooseSchema({
   start: { type: Date, required: true },
-  end: { type: Date, required: true }
+  end: { type: Date, required: true },
 });
