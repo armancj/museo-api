652e4a617b607a5f44b12c1a0b67770988df48bb
armancj <76697120+armancj@users.noreply.github.com>
2024-11-14 09:27:16 -0500

    feat: create crud for cultural heritage property

--- src/cultural-heritage-property/access-and-use-conditions/dto/update-access-and-use-condition.dto.ts
+++ src/cultural-heritage-property/access-and-use-conditions/dto/update-access-and-use-condition.dto.ts
@@ -1,6 +1,12 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAccessAndUseConditionDto } from './create-access-and-use-condition.dto';
 
+/**
+ * DTO for updating access and use conditions.
+ *
+ * This class extends `CreateAccessAndUseConditionDto` using `PartialType`,
+ * making all fields optional for update purposes.
+ */
 export class UpdateAccessAndUseConditionDto extends PartialType(
-  CreateAccessAndUseConditionDto,
+    CreateAccessAndUseConditionDto,
 ) {}
