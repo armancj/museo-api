c9d9cc4e2e7de522769baed61274012a0b44776e
Hectiko <hectikorf@gmail.com>
2025-02-20 09:49:55 -0500

    feat: add authentication to CategoryMuseum controller methods and clean up code formatting

--- src/nomenclator/category-museum/category-museum.controller.ts
+++ src/nomenclator/category-museum/category-museum.controller.ts
@@ -1,45 +1,40 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CategoryMuseumService } from './category-museum.service';
 import { CreateCategoryMuseumDto } from './dto/create-category-museum.dto';
 import { UpdateCategoryMuseumDto } from './dto/update-category-museum.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Auth } from '../../auth/decorator/auth.decorator';
 
 @ApiTags('Category Museum')
 @Controller('category-museum')
 export class CategoryMuseumController {
-  constructor(private readonly categoryMuseumService: CategoryMuseumService) { }
+  constructor(private readonly categoryMuseumService: CategoryMuseumService) {}
 
+  @Auth()
   @Post()
   create(@Body() createCategoryMuseumDto: CreateCategoryMuseumDto) {
     return this.categoryMuseumService.create(createCategoryMuseumDto);
   }
 
+  @Auth()
   @Get()
   findAll() {
     return this.categoryMuseumService.findAll();
   }
 
+  @Auth()
   @Get(':uuid')
   findOne(@Param('uuid') uuid: string) {
     return this.categoryMuseumService.findOne(uuid);
   }
 
+  @Auth()
   @Patch(':uuid')
-  update(
-    @Param('uuid') uuid: string,
-    @Body() updateCategoryMuseumDto: UpdateCategoryMuseumDto,
-  ) {
+  update(@Param('uuid') uuid: string, @Body() updateCategoryMuseumDto: UpdateCategoryMuseumDto) {
     return this.categoryMuseumService.update(uuid, updateCategoryMuseumDto);
   }
 
+  @Auth()
   @Delete(':uuid')
   remove(@Param('uuid') uuid: string) {
     return this.categoryMuseumService.remove(uuid);
--- src/nomenclator/category-museum/category-museum.module.ts
+++ src/nomenclator/category-museum/category-museum.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoryMuseumService } from './category-museum.service';
 import { CategoryMuseumController } from './category-museum.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import {
-  CategoryMuseumNameEntity,
-  CategoryMuseumSchema,
-} from './schema/category-museum.schema';
+import { CategoryMuseumNameEntity, CategoryMuseumSchema } from './schema/category-museum.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: CategoryMuseumNameEntity, schema: CategoryMuseumSchema },
-    ]),
+    MongooseModule.forFeature([{ name: CategoryMuseumNameEntity, schema: CategoryMuseumSchema }]),
   ],
   controllers: [CategoryMuseumController],
   providers: [CategoryMuseumService],
