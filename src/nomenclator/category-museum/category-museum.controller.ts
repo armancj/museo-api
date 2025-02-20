73856d71be73bb2b65b80bb2b531f102377329ec
Hectiko <hectikorf@gmail.com>
2025-02-20 10:34:40 -0500

    feat: restrict access to CategoryMuseum controller methods based on user roles

--- src/nomenclator/category-museum/category-museum.controller.ts
+++ src/nomenclator/category-museum/category-museum.controller.ts
@@ -4,37 +4,39 @@ import { CreateCategoryMuseumDto } from './dto/create-category-museum.dto';
 import { UpdateCategoryMuseumDto } from './dto/update-category-museum.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { Auth } from '../../auth/decorator/auth.decorator';
+import { Roles } from '../../auth/decorator/roles.decorator';
+import { UserRoles } from '../../users/enum/user-roles.enum';
 
 @ApiTags('Category Museum')
 @Controller('category-museum')
 export class CategoryMuseumController {
   constructor(private readonly categoryMuseumService: CategoryMuseumService) {}
 
-  @Auth()
+  @Auth({roles :[UserRoles.administrator,UserRoles.manager,UserRoles.superAdmin]})
   @Post()
   create(@Body() createCategoryMuseumDto: CreateCategoryMuseumDto) {
     return this.categoryMuseumService.create(createCategoryMuseumDto);
   }
 
-  @Auth()
+  
   @Get()
   findAll() {
     return this.categoryMuseumService.findAll();
   }
 
-  @Auth()
+  
   @Get(':uuid')
   findOne(@Param('uuid') uuid: string) {
     return this.categoryMuseumService.findOne(uuid);
   }
 
-  @Auth()
+  @Auth({roles :[UserRoles.administrator,UserRoles.manager,UserRoles.superAdmin]})
   @Patch(':uuid')
   update(@Param('uuid') uuid: string, @Body() updateCategoryMuseumDto: UpdateCategoryMuseumDto) {
     return this.categoryMuseumService.update(uuid, updateCategoryMuseumDto);
   }
 
-  @Auth()
+  @Auth({roles :[UserRoles.administrator,UserRoles.manager,UserRoles.superAdmin]})
   @Delete(':uuid')
   remove(@Param('uuid') uuid: string) {
     return this.categoryMuseumService.remove(uuid);
