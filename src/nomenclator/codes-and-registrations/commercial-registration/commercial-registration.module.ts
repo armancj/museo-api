9d92e2294adb2c12cf7fde0d4b6fddc4f1316250
jennidalila <jrodriguezaloma@gmail.com>
2025-02-23 00:44:41 -0500

    feat: set up CommercialRegistrationModule with controller and service
    Added CommercialRegistrationController and CommercialRegistrationService to the CommercialRegistrationModule, preparing the module for implementing commercial registration functionality.

--- src/nomenclator/codes-and-registrations/commercial-registration/commercial-registration.module.ts
+++ src/nomenclator/codes-and-registrations/commercial-registration/commercial-registration.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CommercialRegistrationController } from './commercial-registration.controller';
+import { CommercialRegistrationService } from './commercial-registration.service';
 
-@Module({})
+@Module({
+  controllers: [CommercialRegistrationController],
+  providers: [CommercialRegistrationService]
+})
 export class CommercialRegistrationModule {}
