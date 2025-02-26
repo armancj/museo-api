6bc0c304ac58efc89d3edfca774f4d118e469a19
mandi <armancanjer@gmail.com>
2025-02-26 15:29:01 -0500

    Refactor CreateUserDto to improve code style and consistency
    Reformatted the CreateUserDto for better alignment and readability, ensuring consistent indentation and spacing. Modified the mobile property validation from `IsMobilePhone` to `IsNumberString` to better reflect its expected format. No functional changes were made to the DTO logic.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,112 +1,111 @@
 import {
-    IsEmail,
-    IsEmpty,
-    IsEnum,
-    IsMobilePhone,
-    IsNotEmpty,
-    IsOptional,
-    IsString,
-    IsUUID,
-    ValidateIf,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  IsUUID,
+  ValidateIf,
 } from 'class-validator';
-import {ApiProperty} from '@nestjs/swagger';
-import {UserPropertiesModel} from '../models/user.model';
-import {UserRoles} from '../enum/user-roles.enum';
-import {ApplyTransform} from "../../common/decorator/apply-transform.decorator";
-import {toLowerCase} from "../../common/utils/to-lower-case";
-import {IsForbiddenForRoles} from "../../auth/decorator/is-forbidden-for-roles-constraint.decorator";
+import { ApiProperty } from '@nestjs/swagger';
+import { UserPropertiesModel } from '../models/user.model';
+import { UserRoles } from '../enum/user-roles.enum';
+import { ApplyTransform } from '../../common/decorator/apply-transform.decorator';
+import { toLowerCase } from '../../common/utils/to-lower-case';
 
 export class CreateUserDto
-    implements Omit<UserPropertiesModel, 'passwordHashed' | 'uuid'> {
-    @IsString()
-    @ApplyTransform(toLowerCase)
-    @IsEmail()
-    @IsNotEmpty()
-    @ApiProperty({
-        type: String,
-        example: 'example@example.com',
-    })
-    email: string;
+  implements Omit<UserPropertiesModel, 'passwordHashed' | 'uuid'>
+{
+  @IsString()
+  @ApplyTransform(toLowerCase)
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    example: 'example@example.com',
+  })
+  email: string;
 
-    @IsMobilePhone()
-    @IsNotEmpty()
-    @ApiProperty({
-        type: String,
-        example: '+34612345678',
-    })
-    mobile: string;
+  @IsNumberString()
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    example: '+34612345678',
+  })
+  mobile: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        type: String,
-        example: 'secretpassword1',
-    })
-    password: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    example: 'secretpassword1',
+  })
+  password: string;
 
-    @IsString()
-    @ApiProperty({
-        type: String,
-        example: 'YourAddressHere',
-    })
-    address: string;
+  @IsString()
+  @ApiProperty({
+    type: String,
+    example: 'YourAddressHere',
+  })
+  address: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        type: String,
-        example: 'YourLastName',
-    })
-    lastName: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    example: 'YourLastName',
+  })
+  lastName: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        type: String,
-        example: 'YourName',
-    })
-    name: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    example: 'YourName',
+  })
+  name: string;
 
-    @ValidateIf((dto) => dto?.roles !== UserRoles.superAdmin)
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        type: String,
-        example: 'Cuba',
-    })
-    nationality: string;
+  @ValidateIf((dto) => dto?.roles !== UserRoles.superAdmin)
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    example: 'Cuba',
+  })
+  nationality: string;
 
-    @ValidateIf((dto) => dto?.roles !== UserRoles.superAdmin)
-    @IsString()
-    @ApiProperty({
-        type: String,
-        example: 'Las Tunas',
-    })
-    province: string;
+  @ValidateIf((dto) => dto?.roles !== UserRoles.superAdmin)
+  @IsString()
+  @ApiProperty({
+    type: String,
+    example: 'Las Tunas',
+  })
+  province: string;
 
-    @ValidateIf(
-        (dto) =>
-            dto?.roles === UserRoles.employee ||
-            dto?.roles === UserRoles.manager,
-    )
-    @IsString()
-    @ApiProperty({
-        type: String,
-        example: 'Jobabo',
-    })
-    municipal: string;
+  @ValidateIf(
+    (dto) =>
+      dto?.roles === UserRoles.employee || dto?.roles === UserRoles.manager,
+  )
+  @IsString()
+  @ApiProperty({
+    type: String,
+    example: 'Jobabo',
+  })
+  municipal: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @IsOptional()
-    @IsEnum(UserRoles)
-    roles?: UserRoles;
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @IsEnum(UserRoles)
+  roles?: UserRoles;
 
-    @ValidateIf((dto) =>
-        dto?.roles === UserRoles.employee || dto?.roles === UserRoles.manager
-    )
-    @IsUUID()
-    @IsString()
-    @IsNotEmpty()
-    institutionId?: string;
+  @ValidateIf(
+    (dto) =>
+      dto?.roles === UserRoles.employee || dto?.roles === UserRoles.manager,
+  )
+  @IsUUID()
+  @IsString()
+  @IsNotEmpty()
+  institutionId?: string;
 }
