0e8e4d0ac195ef79e4ca523d78ec9cbb0b3fda23
mandi <armancanjer@gmail.com>
2025-03-21 15:03:20 -0400

    Refactor Location schema to use `propTypeMongo` utility
    Replaced inline property definitions with the `propTypeMongo` utility function for better code reuse and readability. This change simplifies the schema and ensures consistency across property configurations.

--- src/cultural-heritage-property/entry-and-location-record/schema/location.schema.ts
+++ src/cultural-heritage-property/entry-and-location-record/schema/location.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { LocationModel } from '../models/entry-and-location-record.model';
 import { FieldMetadata } from '../../field-review-status/models/field-review-status.model';
+import { propTypeMongo } from '../../util/prop-type-mongo.function';
 
 /**
  * Mongoose schema for Location.
@@ -8,130 +9,53 @@ import { FieldMetadata } from '../../field-review-status/models/field-review-sta
  */
 @Schema()
 class Location implements LocationModel {
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   floor: FieldMetadata<string>;
 
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   exhibitionRoom: FieldMetadata<string>;
 
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   storage: FieldMetadata<string>;
 
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   showcaseShelf: FieldMetadata<string>;
 
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   shelfDrawer: FieldMetadata<string>;
 
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   box: FieldMetadata<string>;
 
-  @Prop({
-    type: {
-      value: { type: String },
-      status: { type: String, default: 'Pending' },
-      history: {
-        type: [
-          {
-            previousValue: { type: String },
-            modifiedAt: { type: Date },
-            comment: { type: String },
-          },
-        ],
-        default: [],
-      },
-    },
-  })
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
   fileFolder: FieldMetadata<string>;
 }
 
