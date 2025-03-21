cf51e020278e74aab2eabea80c503d8c8712572a
mandi <armancanjer@gmail.com>
2025-03-21 15:01:10 -0400

    Refactor schema to use `FieldMetadata` and `propTypeMongo`.
    Updated multiple fields in the associated documentation schema to leverage the `FieldMetadata` type for enhanced metadata handling. Integrated the `propTypeMongo` utility for a consistent and extensible property definition. These changes standardize schema definitions and improve maintainability.

--- src/cultural-heritage-property/associated-documentation/schema/associated-documentation.ts
+++ src/cultural-heritage-property/associated-documentation/schema/associated-documentation.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { AssociatedDocumentationModel } from '../models/associated-documentation-model';
+import { FieldMetadata } from '../../field-review-status/models/field-review-status.model';
+import { propTypeMongo } from '../../util/prop-type-mongo.function';
 
 /**
  * Mongoose schema class for associated documentation.
@@ -14,8 +16,12 @@ class AssociatedDocumentation implements AssociatedDocumentationModel {
    * This field is optional.
    * @type {string}
    */
-  @Prop()
-  originalsExistenceAndLocation?: string;
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
+  originalsExistenceAndLocation?: FieldMetadata<string>;
 
   /**
    * The existence and location of copies.
@@ -23,8 +29,12 @@ class AssociatedDocumentation implements AssociatedDocumentationModel {
    * This field is required.
    * @type {string}
    */
-  @Prop()
-  copiesExistenceAndLocation: string;
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
+  copiesExistenceAndLocation: FieldMetadata<string>;
 
   /**
    * Information about related description units.
@@ -32,8 +42,12 @@ class AssociatedDocumentation implements AssociatedDocumentationModel {
    * This field is optional.
    * @type {string}
    */
-  @Prop()
-  relatedDescriptionUnits?: string;
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
+  relatedDescriptionUnits?: FieldMetadata<string>;
 
   /**
    * Information regarding related publications.
@@ -41,8 +55,12 @@ class AssociatedDocumentation implements AssociatedDocumentationModel {
    * This field is optional.
    * @type {string}
    */
-  @Prop()
-  relatedPublicationsInformation?: string;
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
+  relatedPublicationsInformation?: FieldMetadata<string>;
 }
 
 /**
