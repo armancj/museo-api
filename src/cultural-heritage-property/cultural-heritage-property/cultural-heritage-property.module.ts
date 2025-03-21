cfd5d4a1cdc5e380e0cbae21dd87777f3eb2f768
mandi <armancanjer@gmail.com>
2025-03-21 15:01:46 -0400

    Remove FieldReviewStatusModule from CH Property module
    The FieldReviewStatusModule import and usage were removed from the CulturalHeritageProperty module. This streamlines dependencies and removes unused or unnecessary modules from the import list.

--- src/cultural-heritage-property/cultural-heritage-property/cultural-heritage-property.module.ts
+++ src/cultural-heritage-property/cultural-heritage-property/cultural-heritage-property.module.ts
@@ -13,7 +13,6 @@ import { AccessAndUseConditionsModule } from '../access-and-use-conditions/acces
 import { AssociatedDocumentationModule } from '../associated-documentation/associated-documentation.module';
 import { DescriptionControlModule } from '../description-control/description-control.module';
 import { CulturalNotesModule } from '../cultural-notes/cultural-notes.module';
-import {FieldReviewStatusModule} from "../field-review-status/field-review-status.module";
 
 @Module({
   imports: [
@@ -33,7 +32,6 @@ import {FieldReviewStatusModule} from "../field-review-status/field-review-statu
     AssociatedDocumentationModule,
     DescriptionControlModule,
     CulturalNotesModule,
-    FieldReviewStatusModule,
   ],
   controllers: [CulturalHeritagePropertyController],
   providers: [CulturalHeritagePropertyService],
