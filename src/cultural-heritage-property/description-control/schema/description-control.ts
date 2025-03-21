9e71b684898faf67f3d43037e0cc9b9761541ed5
mandi <armancanjer@gmail.com>
2025-03-21 15:02:21 -0400

    Refactor schema to use FieldMetadata and propTypeMongo
    Updated `DescriptionControl` properties to use `FieldMetadata` for type consistency and applied `propTypeMongo` for schema definitions. This enhances maintainability and ensures standardized property configuration across the application.

--- src/cultural-heritage-property/description-control/schema/description-control.ts
+++ src/cultural-heritage-property/description-control/schema/description-control.ts
@@ -1,19 +1,37 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { DescriptionControlModel } from '../models/description-control-model';
+import { FieldMetadata } from '../../field-review-status/models/field-review-status.model';
+import { propTypeMongo } from '../../util/prop-type-mongo.function';
 
 @Schema()
 class DescriptionControl implements DescriptionControlModel {
-  @Prop()
-  descriptionMadeBy: string;
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
+  descriptionMadeBy: FieldMetadata<string>;
 
-  @Prop()
-  descriptionDateTime: Date;
+  @Prop(
+    propTypeMongo({
+      type: Date,
+    }),
+  )
+  descriptionDateTime: FieldMetadata<Date>;
 
-  @Prop()
-  reviewedBy: string;
+  @Prop(
+    propTypeMongo({
+      type: String,
+    }),
+  )
+  reviewedBy: FieldMetadata<string>;
 
-  @Prop()
-  reviewDateTime: Date;
+  @Prop(
+    propTypeMongo({
+      type: Date,
+    }),
+  )
+  reviewDateTime: FieldMetadata<Date>;
 }
 export const DescriptionControlSchema =
   SchemaFactory.createForClass(DescriptionControl);
