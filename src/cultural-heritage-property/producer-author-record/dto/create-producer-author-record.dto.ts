343bb9fda2971b4eecb141f044f252d307195753
mandi <armancanjer@gmail.com>
2025-03-21 23:18:18 -0400

    Refactor DTO fields to use FieldMetadataDto type
    Updated all string fields in CreateProducerAuthorRecordDto to use the FieldMetadataDto type, improving metadata handling and consistency. This change ensures enhanced structure and better validation for field-level metadata.

--- src/cultural-heritage-property/producer-author-record/dto/create-producer-author-record.dto.ts
+++ src/cultural-heritage-property/producer-author-record/dto/create-producer-author-record.dto.ts
@@ -1,50 +1,51 @@
 import { ProducerAuthorRecordModel } from '../models/producer-author-record.models';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { FieldMetadataDto } from '../../field-review-status/dto/FieldMetadataDto';
 
 export class CreateProducerAuthorRecordDto
   implements ProducerAuthorRecordModel
 {
   @IsString()
   @IsNotEmpty()
-  betweenStreet1: string;
+  betweenStreet1: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  betweenStreet2: string;
+  betweenStreet2: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  district: string;
+  district: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  institutionalHistory: string;
+  institutionalHistory: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  locality: string;
+  locality: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  municipality: string;
+  municipality: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  number: string;
+  number: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  objectEntryHistory: string;
+  objectEntryHistory: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  producerAuthorNames: string;
+  producerAuthorNames: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  province: string;
+  province: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  street: string;
+  street: FieldMetadataDto<string>;
 }
