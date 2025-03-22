52f4ddf7fdfbffab69990a48ccbe67884dc60fdb
mandi <armancanjer@gmail.com>
2025-03-21 23:13:07 -0400

    Update CreateLocationDto to use FieldMetadataDto for fields
    Refactored the DTO to wrap location fields with `FieldMetadataDto`, enabling enhanced type validation and serialization using `class-transformer` decorators. This change ensures consistency and provides better structure to location-related data handling.

--- src/cultural-heritage-property/entry-and-location-record/dto/create-location.dto.ts
+++ src/cultural-heritage-property/entry-and-location-record/dto/create-location.dto.ts
@@ -1,43 +1,57 @@
 import { LocationModel } from '../models/entry-and-location-record.model';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { FieldMetadataDto } from '../../field-review-status/dto/FieldMetadataDto';
 
 /**
- * DTO for creating a location.
- * @typedef {Object} CreateLocationDto
- * @property {string} box - The box where the item is stored.
- * @property {string} exhibitionRoom - The exhibition room where the item is displayed.
- * @property {string} fileFolder - The file folder where the item is archived.
- * @property {string} floor - The floor where the item is located.
- * @property {string} shelfDrawer - The shelf drawer where the item is kept.
- * @property {string} showcaseShelf - The showcase shelf where the item is exhibit.
- * @property {string} storage - The storage area where the item is stored.
+ * Data Transfer Object (DTO) for creating a new location.
+ * This class serves as a schema for defining, validating,
+ * and transferring data related to a specific location entity.
+ * Implements the structure of the LocationModel interface.
+ *
+ * Each property in the class is type-validated and constrained
+ * to ensure data consistency and correctness. Primarily used in
+ * operations such as creating or validating location-related input data.
+ *
+ * Validation decorators like @IsString and @IsNotEmpty ensure that
+ * the provided data for each field follows the expected format and is not empty.
+ *
+ * The @Type decorator is used to explicitly define the transformation to
+ * the property type, facilitating validation and serialization.
  */
 export class CreateLocationDto implements LocationModel {
   @IsString()
   @IsNotEmpty()
-  box: string;
+  @Type(() => FieldMetadataDto)
+  box: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  exhibitionRoom: string;
+  @Type(() => FieldMetadataDto)
+  exhibitionRoom: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  fileFolder: string;
+  @Type(() => FieldMetadataDto)
+  fileFolder: FieldMetadataDto<string>;
+
+  @IsString()
+  @Type(() => FieldMetadataDto)
+  @IsNotEmpty()
+  floor: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  floor: string;
+  @Type(() => FieldMetadataDto)
+  shelfDrawer: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  shelfDrawer: string;
+  @Type(() => FieldMetadataDto)
+  showcaseShelf: FieldMetadataDto<string>;
 
   @IsString()
   @IsNotEmpty()
-  showcaseShelf: string;
-
-  @IsString()
-  @IsNotEmpty()
-  storage: string;
+  @Type(() => FieldMetadataDto)
+  storage: FieldMetadataDto<string>;
 }
