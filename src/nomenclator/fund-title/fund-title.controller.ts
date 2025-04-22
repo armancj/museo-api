6f52da6112234caa95912e9e6d1c114e6357a8f7
armancj <armancanjer@gmail.com>
2025-04-22 19:20:57 -0400

    refactor: adjust fund-title and access-conditions controllers formatting - fix @ApiTags for fund-title controller - improve import formatting in access-conditions controller - add 'description' field to DescriptionInstrumentEntity

--- src/nomenclator/fund-title/fund-title.controller.ts
+++ src/nomenclator/fund-title/fund-title.controller.ts
@@ -14,8 +14,7 @@ import { UpdateFundTitleDto } from './dto/update-fund-title.dto';
 import { FilterFundTitleDto } from './dto/filter-fund-title.dto';
 import { ApiTags } from '@nestjs/swagger';
 
-//@ApiTags('fund-title')
-@ApiTags('[Nomenclator] fund-title')
+@ApiTags('fund-title')
 @Controller('fund-title')
 export class FundTitleController {
   constructor(private readonly fundTitleService: FundTitleService) {}
